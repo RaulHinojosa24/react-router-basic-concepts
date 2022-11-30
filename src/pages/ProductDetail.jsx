370b69acd21a230ea50f80cd0210787b7f00f8e8
RaulHinojosa24 <raul.hinojosa.perez@gmail.com>
2022-11-30 12:33:34 +0100

    Extracting Route Params

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,7 +1,11 @@
+import { useParams } from "react-router-dom";
+
 const ProductDetail = () => {
+  const params = useParams();
+
   return (
     <section>
-      <h1>Product detail</h1>
+      <h1>Product detail for: {params.productId}</h1>
     </section>
   );
 };
